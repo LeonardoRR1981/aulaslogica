39df0f1b5430941c00ea02bd3af13e4b29317c46
Leonardo <leonardorr81@outlook.com>
2023-09-10 01:42:42 -0300

    exercício 3  de treinamentos de logica

--- logica13.py
+++ logica13.py
@@ -1,3 +1,5 @@
+#Desenvolva um algoritmo que converta uma temperatura em Celsius (C) para Fahrenheit (F)
+
 c = float(input('Quantos graus Celsius? '))
 f = (9 * c / 5) + 32 
 print('Em Fahrenheit são {}'.format(f))
