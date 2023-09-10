d6eccc5b40b14909bd6a250ca42b50561658ac79
Leonardo <leonardorr81@outlook.com>
2023-09-09 23:33:16 -0300

    terceiro commit

---
+++ logica9.py
@@ -0,0 +1,9 @@
+# Crie uma variável de string que receba o seu nome completo. Crie uma 
+# segunda variável, agora do tipo booleana. Esta variável deverá receber o 
+# resultado da comparação lógica que verifica se o tamanho do seu nome é menor 
+# ou igual ao valor 15. Imprima a variável booleana na tela.
+
+nome = 'Leonardo Rodrigues Rosa'
+tam = len(nome)
+com = tam <= 15
+print(com)
