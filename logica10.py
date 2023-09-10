5c65a421a007fb72d9087906c56a2a8c1230ed6b
Leonardo <leonardorr81@outlook.com>
2023-09-10 00:06:34 -0300

    exercício de treinamentos de logica

---
+++ logica10.py
@@ -0,0 +1,8 @@
+# Desenvolva um algoritmo que solicite ao usuário dois números inteiros. Imprima a soma destes dois números na tela.
+  
+n1 = int(input('Entre com um número inteiro: ') )  
+n2 = int(input('Entre com outro número interiro: '))
+print('A soma de {} com {} e: {}'.format(n1, n2, n1 + n2))
+                                            
+
+
